5c8908d036f47e78ebcf3e8a30e109f49fc75bb0
Alexander Ivanovsky <alxiw@outlook.com>
2026-05-31 22:48:29 +0300

    Update gradle files

--- simplesearchview/build.gradle.kts
+++ simplesearchview/build.gradle.kts
@@ -6,26 +6,20 @@ android {
     namespace = "io.github.alxiw.simplesearchview"
     compileSdk = 37
 
-    defaultConfig.minSdk = 23
+    defaultConfig {
+        minSdk = 23
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-        }
     }
 
     buildFeatures.viewBinding = true
 }
 
 dependencies {
-
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation(libs.kotlin.stdlib.jdk8)
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
